cb933f23576272818cadb33fa87021a3e6195633
FlashLink Release <release@7a.cc.cd>
2026-07-14 20:36:04 +0800

    Refresh FlashLink Android launcher icon for 2.6.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,8 +58,8 @@ subprojects {
             minSdk = 21
             targetSdk = 35
 
-            versionName = "2.6.1"
-            versionCode = 20601
+            versionName = "2.6.2"
+            versionCode = 20602
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
